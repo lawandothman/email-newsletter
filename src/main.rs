7b0b466fc05c09c719cedae2b60168f92b855778
Lawand Othman <lwnd@pm.me>
2022-11-30 07:17:13 +0000

    use random port for testing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
+
 use email_newsletter::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind por");
+    run(listener)?.await
 }
