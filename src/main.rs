7bcaa7cfa685e526fae13f64fcc218a5d46d0e56
Lawand Othman <lwnd@pm.me>
2022-11-30 13:01:25 +0000

    read values from configuration.yml

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::net::TcpListener;
 
-use email_newsletter::startup::run;
+use email_newsletter::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind por");
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
